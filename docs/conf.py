848caba69b5a59a730699f3464d96099ae09b4c6
Leif Walsh <leif.walsh@gmail.com>
2017-08-26 13:11:25 -0400

    added intersphinx

--- docs/conf.py
+++ docs/conf.py
@@ -280,3 +280,6 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None),
+                       'pandas': ('https://pandas.pydata.org/pandas-docs/stable/', None)}
